6f21d420bac7d688e9aff95ead60d382ff2d4a22
dancaprima <dancaprimaa@gmail.com>
2017-01-09 01:24:38 +0700

    Finish input data to array

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 //Budget Controller
 var budgetController = (function () {
-    
-    var Expense = function(id, description, value){
+
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    
-    var Income = function(id, description, value){
+    var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
+    var data = {
+        allItems: {
+            expense: [],
+            income: []
+        },
+        totals: {
+            expense: [],
+            incomee: []
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, id;
+
+
+            if(data.allItems[type].length > 0 ){
+                id = data.allItems[type][data.allItems[type].length - 1 ].id + 1;
+            }else {
+                id = 0;
+            }
+
+            if (type === 'expense') {
+                 newItem = new Expense(id, des, val)
+            } else if (type === 'income') {
+                 newItem = new Income(id, des, val)
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem
+        },
+
+        testing: function(){
+            console.log(data);
+        }
+    }
 })();
-
-
 
 //UI Controller
 var UIController = (function () {
@@ -54,7 +87,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     var setupEventListeners = function () {
         var DOM = UICtrl.getDOMStrings();
-        
+
         document
             .querySelector(DOM.inputBtn)
             .addEventListener('click', ctrlAdditem);
@@ -67,22 +100,26 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     }
 
-   
-
     var ctrlAdditem = function () {
+        var input, newItem;
         //1.Get Input Data
-        var input = UICtrl.getinput();
+        input = UICtrl.getinput();
 
-        // 2.Add the item to the budget controller 3.Add the items to the UI 4.Calculate
-        // the budget 5.Display the budget on the UI
+        // 2.Add the item to the budget controller 
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        //3.Add the items to the UI 
+        
+        //.4.Calculate the budget 
+        
+        //5.Display the budget on the UI
 
     }
 
     return {
-        init: function(){
+        init: function () {
             console.log('Application has started');
             setupEventListeners();
-        },
+        }
     }
 
 })(budgetController, UIController);
